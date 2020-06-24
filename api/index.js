57be7dccfcebc01438d1cfdc04c8340f7906a565
Ed Addario <eaddario@hotmail.com>
2020-06-24 23:43:49 +0100

    Added worker horizontal scalability

--- api/index.js
+++ api/index.js
@@ -1,70 +1,83 @@
-const keys = require('./keys');
+const config = require('./config')
 
 //Express app setup
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
 //Postgres app setup
-const { Pool } = require('pg');
+const {Pool} = require('pg')
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
-});
+    database: config.Postgres.Database,
+    host: config.Postgres.Host,
+    port: config.Postgres.Port,
+    user: config.Postgres.User,
+    password: config.Postgres.Password
+})
 
-pgClient.on('error', () => console.log('Lost Postgres connection'));
-
-pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch(err => console.log(err));
+pgClient.on('error', () => console.log('Lost Postgres connection'))
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err))
 
 //Redis client setup
-const redis = require('redis');
+const redis = require('redis')
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
+    host: config.Redis.Host,
+    port: config.Redis.Port,
     retry_strategy: () => 1000
-});
-const redisPublisher = redisClient.duplicate();
+})
+//const redisPublisher = redisClient.duplicate()
+
+// RabbitMQ client setup
+const rabbitmq = require('rascal').Broker
+let rabbitClient
+rabbitmq.create(config.RabbitMQ, (err, _rabbitClient) => {
+    if (err) throw err
+    rabbitClient = _rabbitClient
+})
 
 //Express route handlers
 app.get('/', (req, res) => {
-    res.send('API Running...');
+    res.send('API Running...')
 });
 
 app.get('/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * FROM values');
-    res.send(values.rows);
-});
+    res.send(values.rows)
+})
 
 app.get('/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
-        res.send(values);
-    });
-});
+        res.send(values)
+    })
+})
 
 app.post('/values', async (req, res) => {
-    const index = parseInt(req.body.index);
+    const index = parseInt(req.body.index)
 
     if (!(Number.isInteger(index)) || (index > 50)) {
-        return res.status(422).send('Index too high');
+        return res.status(422).send('Index too high')
     }
 
-    redisClient.hset('values', index, 'NaN');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+    redisClient.hset('values', index, 'NaN')
+    //redisPublisher.publish('insert', index)
 
-    res.send({working: true});
-});
+    rabbitClient.publish('worker_pub', index, (err, pub) => {
+        if (err) throw err
+        pub.on('error', console.error)
+    })
+    console.log('Published ' + index)
+
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+
+    res.send({working: true})
+})
 
 //Express instantiation
 app.listen(5000, err => {
-    console.log('API listening...');
-});
+    console.log('API listening...')
+})
--- worker/index.js
+++ worker/index.js
@@ -1,25 +1,37 @@
-const keys = require('./keys');
-const redis = require('redis');
+const config = require('./config')
+const redis = require('redis')
+const Broker = require('rascal').Broker
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
+    host: config.Redis.Host,
+    port: config.Redis.Port,
     retry_strategy: () => 1000
-});
+})
 
-const redisSubscriber = redisClient.duplicate();
-
-//Inefficient Fibonacci algorithm used on purpose to simulate different workloads
+//Inefficient Fibonacci algorithm used to simulate system behaviour under different workloads
 function fib(index) {
-    if (index < 2) return 1;
-    return (fib(index - 1) + fib(index - 2));
+    if (index < 2) return 1
+    return (fib(index - 1) + fib(index - 2))
 }
 
-redisSubscriber.on('message', (channel, message) => {
-    const time = process.hrtime();
-    redisClient.hset('values', message, fib(parseInt(message)));
-    const diff = process.hrtime(time);
-    console.log(`Computed Fibonacci for index ${message} in ${diff[0]}s ${diff[1] / 1000000}ms`);
-});
+Broker.create(config.RabbitMQ, (err, broker) => {
+    if (err) throw err
 
-redisSubscriber.subscribe('insert');
+    // Consume a message
+    broker.subscribe('worker_sub', (err, sub) => {
+        if (err) throw err
+
+        sub.on('message', (message, content, ackOrNack) => {
+            ackOrNack()
+            console.log(`Got message ${content}`)
+            const time = process.hrtime()
+            redisClient.hset('values', content, fib(parseInt(content)))
+            const diff = process.hrtime(time)
+            console.log(`Computed Fibonacci for index ${content} in ${diff[0]}s ${diff[1] / 1000000}ms`)
+        })
+            .on('error', console.error)
+            .on('cancel', console.warn)
+    });
+
+    broker.on('error', console.error)
+})
